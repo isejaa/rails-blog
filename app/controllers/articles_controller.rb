6eb50bab2f94aa91d2e43462e2c607bbb9e5630b
Humberto Barrientos <Hawksmoor@gmail.com>
2017-12-15 21:20:19 +0100

    Edition of an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def show
@@ -13,9 +14,26 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+def update
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
   private
     def article_params
       params.require(:article).permit(:title, :text)
