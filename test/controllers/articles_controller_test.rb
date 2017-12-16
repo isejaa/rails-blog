35aafc1ec2018ff73ca58b813518bc8d1840b945
Humberto Barrientos <Hawksmoor@gmail.com>
2017-12-16 02:45:00 +0100

    Added some test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get articles_url
+    assert_response :success
+  end
+
+  test "should create article" do
+    assert_difference('Article.count') do
+      post articles_url, params: { article: { body: 'Rails is awesome!', title: 'Hello Rails' } }
+    end
+
+    assert_redirected_to article_path(Article.last)
+  end
 end
