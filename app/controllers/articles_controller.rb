0ca6e9570f6a4c37f02b3842d80bf39ec554b61d
Humberto Barrientos <Hawksmoor@gmail.com>
2017-12-16 02:09:01 +0100

    Delete an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,15 +25,23 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-def update
-  @article = Article.find(params[:id])
+  def update
+    @article = Article.find(params[:id])
 
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
-end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
